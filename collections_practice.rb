68eb18403822888b6e81bf006c19b53c44b47e95
Amy Westcott <amy.westcott@yahoo.com>
2019-06-11 20:48:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,16 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |word|
+    
+end
+def find_a(array)
+end
+def sum_array(array)
+end
+def add_s(array)
+end
